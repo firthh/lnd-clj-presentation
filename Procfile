aa9f92918c1a84292b660e72e734d2ad0ad20913
Hugo Firth <hugo.firth@uswitch.com>
2016-03-29 19:50:55 +0100

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/presentation-rater.jar clojure.main -m presentation-rater.server
---
+++ env/prod/clj/presentation_rater/middleware.clj
@@ -0,0 +1,5 @@
+(ns presentation-rater.middleware
+  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+
+(defn wrap-middleware [handler]
+  (wrap-defaults handler site-defaults))
